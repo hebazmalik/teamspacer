c005d9394a1b8e001eaf4849d3074b0f5fe543d5
Tim White <tim94white@gmail.com>
2021-06-08 13:00:43 +0100

    rails controller, model and views

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,4 +2,6 @@ class Space < ApplicationRecord
   belongs_to :area
   has_many :plans
   has_many :amenities
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
