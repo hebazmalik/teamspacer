f57c8adb7e836d1ecb79e6c786ca3d01e279f0fd
Heba Malik <hebazmalik@gmail.com>
2021-06-08 16:51:14 +0100

    added images to spaces

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -2,11 +2,17 @@ class SpacesController < ApplicationController
 
   def show
     @space = Space.find(params[:id])
-  end 
+  end
 
   def index
     @area = Area.find(params[:area_id])
     @spaces = Space.where(area: @area)
-  end 
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:name, :location, :description, :photo)
+  end
 
 end
