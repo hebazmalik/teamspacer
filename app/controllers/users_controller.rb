8c4ea10e984b39f013159ea096aff53c48e53f22
Tim White <tim94white@gmail.com>
2021-06-14 17:15:33 +0100

    favorites user show page tab

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @plans = @user.plans
+
+    @favorite_spaces = @user.favorites_by_type('Space').map(&:favoritable)
+    @favorite_restaurants = @user.favorites_by_type('Restaurant').map(&:favoritable)
   end
 
   # def acts_as_favoritable
